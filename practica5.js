84eb7a459b37166f7368f68867a5224877dc2674
Abner Jimenez <ajjg21@gmail.com>
2023-08-22 12:49:09 -0600

    venta de alcohol

---
+++ practica5.js
@@ -0,0 +1,9 @@
+// Solicitar la edad al usuario
+let edad = parseInt(prompt("Por favor, ingresa tu edad:"));
+
+// Verificar si la edad es mayor o igual a 18
+if (edad >= 18) {
+    alert("Puedes comprar cerveza. ¡Disfruta con responsabilidad!");
+} else {
+    alert("Lo siento, debes ser mayor de 18 años para comprar cerveza.");
+}
